91cf1c8d0ff9363e92ff642f882346ca3b5c40ef
cadenzah <cadenzah93@gmail.com>
2022-01-22 03:19:39 +0900

    add ESLint + Prettier
    - run eslint when running dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        eslintPlugin(),
         reactRefresh()
     ]
 });
