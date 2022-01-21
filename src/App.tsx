f8ada9d156821bccc2884cb1a96c698631405248
cadenzah <cadenzah93@gmail.com>
2022-01-22 03:12:23 +0900

    bootstraping
    - react
    - typescript
    - vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <p>Hello, React with Vite!</p>
+        </div>
+    );
+}
+
+export default App;
